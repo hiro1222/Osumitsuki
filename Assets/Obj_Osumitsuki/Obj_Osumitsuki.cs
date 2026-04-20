9b4064d3a51e8c97cb3153f82f349e53d7ee4b6b
HUU726 <huuki0726@icloud.com>
2026-04-20 21:53:38 +0900

    Obj_Osumitsukiの外枠制作中 インクの容量 インクの現在量 インクパーセンテージ を制作

---
+++ Assets/Obj_Osumitsuki/Obj_Osumitsuki.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Obj_Osumitsuki : MonoBehaviour
+{
+
+    [Header("インクステータス")]
+    //現在のインクの量
+    private float curInkAmount;
+    [SerializeField] private float maxInkCapa = 100;      //インクの最大量
+    [SerializeField] private float InkRatio = 70;   //お墨付き
+
+    private bool osumitsukiFlg;
+
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        curInkAmount = 0;
+        osumitsukiFlg = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
