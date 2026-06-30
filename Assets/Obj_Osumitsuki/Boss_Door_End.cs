9ad8291d9097c8b1c2b8d6282d6372079f460eef
HUU726 <146161598+HUU726@users.noreply.github.com>
2026-06-30 23:39:43 +0900

    エフェクトのテクスチャが張り替わる問題をハードコードで名前指定に変更

--- Assets/Obj_Osumitsuki/Boss_Door_End.cs
+++ Assets/Obj_Osumitsuki/Boss_Door_End.cs
@@ -77,7 +77,7 @@ public class Boss_Door : MonoBehaviour
 			foreach (Transform child in children)
 			{
 				if (transform == child) continue;
-				if (child.gameObject.name == auraEffect.name) continue;
+				if (child.gameObject.name == "smoke_aura(Clone)") continue;
 				Renderer rend = child.GetComponent<Renderer>();
 				if (rend == null) continue;
 				rend.material = afferMat;
